dd99ab0634e3536739e97938053e514e94e77e5c
Kyle Frazier <kyle.matthew.frazier@gmail.com>
2020-10-08 15:30:48 -0600

    Added an infinite loop conditional to authenticator status

--- 18-travisci/virtualization.ts
+++ 18-travisci/virtualization.ts
@@ -1,33 +1,81 @@
-import { Stage, Event, metronome, normal, TimedDependency, FIFOQueue } from "../../src";
+import { Stage, Event, metronome, normal, standardNormal, TimedDependency, FIFOQueue } from "../../src";
 
+export class Virtualization extends Stage {
+    public queAuthenticator: boolean;
+    constructor(protected wrapped: Stage) {
+      super();
+      this.inQueue = new FIFOQueue(Infinity, 220); //queue length ( (Events a worker can run), (number of workers) )
+      this.queAuthenticator = true;
+    }
+
+    async workOn(event: Event): Promise<void> {
+      await this.build(event);
+      await this.run(event);
+      await this.cleanup(event);
+    }
+
+    async build(event: Event): Promise<void>  {
+      const latency = normal(10, 2);
+      console.log("BUILD");
+      await metronome.wait(latency);
+    }
+
+    async run(event: Event): Promise<void>  {
+      const latency = normal(10, 2);
+      console.log("RUN");
+      await metronome.wait(latency);
+    }
+
+    async cleanup(event: Event): Promise<void>  {
+      const latency = normal(10, 2);
+      console.log("CLEANUP " + this.queAuthenticator);
+      //await metronome.wait(latency);
+      
+      if (this.queAuthenticator == true) {
+        await metronome.wait(latency);
+      }
+      else {
+        await metronome.wait(Infinity);
+      }
+
+      // sometimes don't return from this function. (You can probably do this by setting caling await.metronome.wait(Infinity)
+    }
+}
+
+
+/*
 export class VirtualStage extends TimedDependency {
     constructor(protected wrapped: Stage) {
         super();
-        
+
     }
 
     async workOn(event: Event) {
-        try {await this.Build(event);}
-        catch {console.log("Error in Build");}
-        await this.Run(event);
-        await this.Cleanup(event);
+        try {await this.build(event);}
+        catch {console.log("Error in workOn");}
+        await this.run(event);
+        await this.cleanup(event);
+        //await this.Build(this.Run((this.Cleanup(event))));
     }
 
-    async Build(event: Event) {
+    async build(event: Event) {
+        console.log("BUILD");
         const latency = normal(8, 2); //latency between 6 and 10
         await metronome.wait(latency);
         this.inQueue = new FIFOQueue(1, 50); //queue length; ( (Events a worker can run), (number of workers) )
         await this.wrapped.accept(event);
     } // Build
 
-    async Run(event: Event) {
+    async run(event: Event) {
+        console.log("RUN");
         const latency = normal(8, 2); //latency between 6 and 10
         await metronome.wait(latency);
         this.inQueue = new FIFOQueue(1, 50); //queue length; ( (Events a worker can run), (number of workers) )
         await this.wrapped.accept(event);
     } // Run
 
-    async Cleanup(event: Event) {
+    async cleanup(event: Event) {
+        console.log("CLEANUP");
         const latency = normal(8, 2); //latency between 6 and 10
         await metronome.wait(latency);
         this.inQueue = new FIFOQueue(1, 50); //queue length; ( (Events a worker can run), (number of workers) )
@@ -35,3 +83,4 @@ export class VirtualStage extends TimedDependency {
     } // Cleanup
 
 }
+*/
