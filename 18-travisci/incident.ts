4ed599ffca1883064085557013b6ee2613c3457e
Kyle Frazier <kyle.matthew.frazier@gmail.com>
2020-10-15 16:24:09 -0600

    Initialized the new incident stages, added descriptive comments

--- 18-travisci/incident.ts
+++ 18-travisci/incident.ts
@@ -10,65 +10,70 @@
  */
 
 
-import { 
-    Stage, 
-    Event, 
-    metronome, 
-    normal, 
-    simulation,
-    stats,
-    eventSummary, 
-    stageSummary,
-    Timeout,
-    TimeStats
+import {
+  Stage,
+  Event,
+  metronome,
+  normal,
+  simulation,
+  stats,
+  eventSummary,
+  stageSummary,
+  Timeout,
+  TimeStats,
+  Retry
 } from "../../src";
-import { BuildService } from "./build-service";
+import { Intake } from "./intake";
 import { Virtualization } from "./virtualization";
 
-// initialize build-service
-const build = new BuildService();
-
-// pass build-service as a parameter into virtualization
-const vir = new Virtualization(build);
+// Drives the events' failures.
+const vir = new Virtualization();
 
 // Gives timeout to end simulation after a period of time.
 const timeout = new Timeout(vir);
 
+// Wraps timeout stage and resubmits events upon failure.
+const retry = new Retry(timeout);
+
+// Sends events through a queue.
+const intake = new Intake(retry);
+
+timeout.timeout = 20000; // times out after x ticks.
+
 // scenario
 simulation.keyspaceMean = 1000;
-simulation.keyspaceStd = 200; // 68% - 1000 +/- 200    97% - 1000 +/- 400     99% 1000 +/- 600 
+simulation.keyspaceStd = 200;
 simulation.eventsPer1000Ticks = 1000;
-//timeout.timeout = 20000; // times out after x ticks.
 
-//Initializes the flow of events.
+// Initializes the flow of events.
 async function work() {
-  const events = await simulation.run(vir, 5000); // (destination, total events sent).
+  const events = await simulation.run(intake, 40000); // (destination, total events sent).
   console.log("done");
   //stats.summary();
   eventSummary(events);
-  stageSummary([/*timeout, */vir, build]) //In output: "Overview of event time spent in stage" and "...behavior in stage", prints info of vir.
+  stageSummary([intake, retry, timeout, vir]);
   stats.summary();
   const times = events.map(e => e.stageTimes);
   console.log(times[0]);
-  metronome.stop(true); //this will stop the timer.
-  //metronome.debug(true) //will show when functions will execute 
+
 }
 work();
 
-metronome.setTimeout(breakVir, 3000); //breaks authenticator in Virtualization after 1000 ticks
+// Breaks cleanup function in Virtualization after x ticks
+// After setting virtual's cleanupVM to fail, the cleanup will cause resourcesUsed to accumulate.
+metronome.setTimeout(breakVir, 5000); 
 
-//After setting virtual's que authentication to fail, the cleanup cannot service events.
 function breakVir() {
-  vir.queAuthenticator = false;
+  vir.janitorProcessWorking = false;
 }
 
 function poll() {
-    const now = metronome.now();
-    const eventRate = simulation.getArrivalRate();
-    //const eventTime = TimeStats.fromStage(vir); //prints string of TimeStats
-  
-    stats.record("poll", {
-      now, eventRate, //eventTime
-    });
-  }
-  metronome.setInterval(poll, 1000);
+  const now = metronome.now();
+  const eventRate = simulation.getArrivalRate();
+  //const eventTime = TimeStats.fromStage(vir); //prints string of TimeStats
+
+  stats.record("poll", {
+    now, eventRate, //eventTime
+  });
+}
+metronome.setInterval(poll, 1000);
